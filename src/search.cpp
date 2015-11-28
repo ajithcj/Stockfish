3bcacbe8664894d7749d07a6e0bb41f21ddcb584
Ajith <achajo@yahoo.co.in>
2015-11-29 01:39:37 +0530

    don't shallow prune moves if it has a positive history

--- src/search.cpp
+++ src/search.cpp
@@ -917,8 +917,7 @@ moves_loop: // When in check search starts from here
           // Move count based pruning
           if (   depth < 16 * ONE_PLY
               && moveCount >= FutilityMoveCounts[improving][depth]
-              && !(thisThread->history[pos.moved_piece(move)][to_sq(move)] > VALUE_ZERO
-		   || cmh[pos.moved_piece(move)][to_sq(move)] > VALUE_ZERO))
+              && thisThread->history[pos.moved_piece(move)][to_sq(move)] <= VALUE_ZERO)
               continue;
 
           // History based pruning
