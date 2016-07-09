80d8b6278cac5651a9277b894e0f87e595e4b58c
ajithcj <achajo@yahoo.co.in>
2016-07-09 17:58:34 +0000

    relax staticEval check as go to higher depths

--- src/search.cpp
+++ src/search.cpp
@@ -731,7 +731,7 @@ namespace {
     if (   !PvNode
         &&  depth >= 2 * ONE_PLY
         &&  eval >= beta
-        && (ss->staticEval >= beta || depth >= 12 * ONE_PLY)
+        && (ss->staticEval >= beta - ((int) depth - 2) * 20)
         &&  pos.non_pawn_material(pos.side_to_move()))
     {
         ss->currentMove = MOVE_NULL;
