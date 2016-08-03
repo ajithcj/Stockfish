97b1d2eb4c709158f4403c20ba15b81911d75bf6
ajithcj <achajo@yahoo.co.in>
2016-08-03 17:08:34 +0000

    Penalize safe checks even if there are no attackers to kingring

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -470,6 +470,38 @@ namespace {
         // array and subtract the score from the evaluation.
         score -= KingDanger[std::max(std::min(attackUnits, 399), 0)];
     }
+    else
+    {
+        // Analyse the safe enemy's checks which are possible on next move...
+        safe  = ~(ei.attackedBy[Us][ALL_PIECES] | pos.pieces(Them));
+
+        b1 = pos.attacks_from<ROOK  >(ksq);
+        b2 = pos.attacks_from<BISHOP>(ksq);
+
+        // Enemy queen safe checks
+        if ((b1 | b2) & ei.attackedBy[Them][QUEEN] & safe)
+            score -= SafeCheck;
+
+        // For other pieces, also consider the square safe if attacked twice,
+        // and only defended by a queen.
+        safe |=  ei.attackedBy2[Them]
+               & ~(ei.attackedBy2[Us] | pos.pieces(Them))
+               & ei.attackedBy[Us][QUEEN];
+
+        // Enemy rooks safe and other checks
+        if (b1 & ei.attackedBy[Them][ROOK] & safe)
+            score -= SafeCheck;
+
+        // Enemy bishops safe and other checks
+        if (b2 & ei.attackedBy[Them][BISHOP] & safe)
+            score -= SafeCheck;
+
+        // Enemy knights safe and other checks
+        b = pos.attacks_from<KNIGHT>(ksq) & ei.attackedBy[Them][KNIGHT];
+        if (b & safe)
+            score -= SafeCheck;
+
+    }
 
     if (DoTrace)
         Trace::add(KING, Us, score);
