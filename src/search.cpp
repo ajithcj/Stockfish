6fc79a1254e929cc7b25c8909a9b119110b3f02c
ajithcj <achajo@yahoo.co.in>
2016-09-02 05:17:22 +0000

    less reduction for evasion only if see is not negative

--- src/search.cpp
+++ src/search.cpp
@@ -1007,8 +1007,9 @@ moves_loop: // When in check search starts from here
               else if (   type_of(move) == NORMAL
                        && type_of(pos.piece_on(to_sq(move))) != PAWN
                        && see_sign 
-		       && pos.see(make_move(to_sq(move), from_sq(move))) < VALUE_ZERO)
+                       && pos.see(make_move(to_sq(move), from_sq(move))) < VALUE_ZERO)
                   r -= 2 * ONE_PLY;
+
               // Decrease/increase reduction for moves with a good/bad history
               Value val = thisThread->history[moved_piece][to_sq(move)]
                          +    (cmh  ? (*cmh )[moved_piece][to_sq(move)] : VALUE_ZERO)
