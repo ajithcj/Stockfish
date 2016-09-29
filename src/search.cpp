cfc2b7e9457dcdb30fc43c94b567b165f5568579
ajithcj <achajo@yahoo.co.in>
2016-09-29 13:17:22 +0000

    take 2

--- src/search.cpp
+++ src/search.cpp
@@ -743,7 +743,7 @@ namespace {
         &&  depth < 7 * ONE_PLY
         &&  eval - futility_margin(depth) >= beta
         &&  eval < VALUE_KNOWN_WIN  // Do not return unproven wins
-        && (ss->staticEval >= beta - 35 * (depth / ONE_PLY - 6))
+        && (ss->staticEval >= beta + 35 * (depth / ONE_PLY))
         &&  pos.non_pawn_material(pos.side_to_move()))
         return eval - futility_margin(depth);
 
