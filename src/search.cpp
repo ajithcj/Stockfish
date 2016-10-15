0e90e49c47a7ea3141fad4a9559d72f955359935
ajithcj <achajo@yahoo.co.in>
2016-10-15 08:59:25 +0000

    Null move pruning check

--- src/search.cpp
+++ src/search.cpp
@@ -748,7 +748,6 @@ namespace {
     // Step 8. Null move search with verification search (is omitted in PV nodes)
     if (   !PvNode
         &&  eval >= beta
-        && (ss->staticEval >= beta - 35 * (depth / ONE_PLY - 6) || depth >= 13 * ONE_PLY)
         &&  pos.non_pawn_material(pos.side_to_move()))
     {
         ss->currentMove = MOVE_NULL;
@@ -759,6 +758,8 @@ namespace {
         // Null move dynamic reduction based on depth and value
         Depth R = ((823 + 67 * depth / ONE_PLY) / 256 + std::min((eval - beta) / PawnValueMg, 3)) * ONE_PLY;
 
+        if((((depth-R) >= ONE_PLY) && (depth >= 13*ONE_PLY || ss->staticEval >= beta - 110)) || (ss->staticEval >= beta + 150)) 
+        {
         pos.do_null_move(st);
         (ss+1)->skipEarlyPruning = true;
         nullValue = depth-R < ONE_PLY ? -qsearch<NonPV, false>(pos, ss+1, -beta, -beta+1, DEPTH_ZERO)
@@ -784,6 +785,7 @@ namespace {
             if (v >= beta)
                 return nullValue;
         }
+	}
     }
 
     // Step 9. ProbCut (skipped when in check)
