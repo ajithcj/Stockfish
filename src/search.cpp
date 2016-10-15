8cc3a75d5074565d2b9d4cac47c83600a76730c3
ajithcj <achajo@yahoo.co.in>
2016-10-15 17:15:43 +0000

    Use Static Eval for NMP depth reduction

--- src/search.cpp
+++ src/search.cpp
@@ -757,7 +757,7 @@ namespace {
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and value
-        Depth R = ((823 + 67 * depth / ONE_PLY) / 256 + std::min((eval - beta) / PawnValueMg, 3)) * ONE_PLY;
+        Depth R = ((823 + 67 * depth / ONE_PLY) / 256 + std::max(0, std::min((ss->staticEval - beta) / PawnValueMg, 3))) * ONE_PLY;
 
         pos.do_null_move(st);
         (ss+1)->skipEarlyPruning = true;
