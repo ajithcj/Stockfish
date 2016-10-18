41a6c95ef06a0c3079595b1cdd33e59a75124fb1
ajithcj <achajo@yahoo.co.in>
2016-10-18 14:12:55 +0000

    What if we use a constant futility margin

--- src/search.cpp
+++ src/search.cpp
@@ -66,7 +66,7 @@ namespace {
 
   // Razoring and futility margin based on depth
   const int razor_margin[4] = { 483, 570, 603, 554 };
-  Value futility_margin(Depth d) { return Value(150 * d / ONE_PLY); }
+  Value futility_margin(Depth d) { return Value(150); }
 
   // Futility and reductions lookup tables, initialized at startup
   int FutilityMoveCounts[2][16]; // [improving][depth]
